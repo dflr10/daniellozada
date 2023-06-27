8c43b8f5483f7ac18b63a5696d609537a6f3fb8f
dflr10 <dafelor.10@hotmail.com>
2023-06-27 12:11:19 -0500

    variable green-dark added

--- src/config/content-config.js
+++ src/config/content-config.js
@@ -1,15 +1,15 @@
 const dotenv = require("dotenv");
   
 let activeEnviroment = process.env.GATSBY_ACTIVE_ENV || "development";
+console.log('!!!activeEnviroment: '+ activeEnviroment);
 
 if (activeEnviroment === "development") {
   dotenv.config({ path: './src/env/.env.development'});
 } else {
   (activeEnviroment === "staging")
-      ? dotenv.config({path: './src/env/.env.integration'})
-      : dotenv.config({path: './src/env/.env'})
+    ? dotenv.config({path: './src/env/.env.integration'})
+    : dotenv.config({path: './src/env/.env'})
 }
-
 
 module.exports = {
         resolve: `gatsby-source-contentful`,
